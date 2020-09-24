9ddbbdb61e1887b4242ecb57b6b427007e3dfebf
billbo99 <cactus_computers-github@yahoo.com>
2020-09-24 20:11:33 +0100

    1.1.0

--- dist/spawn.lua
+++ dist/spawn.lua
@@ -33,31 +33,33 @@ end
 -- On new player created setup some default gear
 function Spawn.GiveGear(event)
     local player = game.get_player(event.player_index)
+    local force = player.force
+    local ref = global.SpawnForceItems[force.name]
 
     Spawn.ClearPlayerInventories(player)
 
-    if settings.get_player_settings(player)["billbo99-respawn-with-primary_gun"] and global.SpawnItems["primary_gun"] then
-        player.insert {name = global.SpawnItems["primary_gun"], count = 1}
+    if settings.get_player_settings(player)["billbo99-respawn-with-primary_gun"] and ref["primary_gun"] then
+        player.insert {name = ref["primary_gun"], count = 1}
     end
 
-    if settings.get_player_settings(player)["billbo99-respawn-with-primary_ammo"] and global.SpawnItems["primary_ammo"] then
-        player.insert {name = global.SpawnItems["primary_ammo"], count = settings.global["billbo99-primary_ammo_starting_amount"].value}
+    if settings.get_player_settings(player)["billbo99-respawn-with-primary_ammo"] and ref["primary_ammo"] then
+        player.insert {name = ref["primary_ammo"], count = settings.global["billbo99-primary_ammo_starting_amount"].value}
     end
 
-    if settings.get_player_settings(player)["billbo99-respawn-with-secondary_gun"] and global.SpawnItems["secondary_gun"] then
-        player.insert {name = global.SpawnItems["secondary_gun"], count = 1}
+    if settings.get_player_settings(player)["billbo99-respawn-with-secondary_gun"] and ref["secondary_gun"] then
+        player.insert {name = ref["secondary_gun"], count = 1}
     end
 
-    if settings.get_player_settings(player)["billbo99-respawn-with-secondary_ammo"] and global.SpawnItems["secondary_ammo"] then
-        player.insert {name = global.SpawnItems["secondary_ammo"], count = settings.global["billbo99-secondary_ammo_starting_amount"].value}
+    if settings.get_player_settings(player)["billbo99-respawn-with-secondary_ammo"] and ref["secondary_ammo"] then
+        player.insert {name = ref["secondary_ammo"], count = settings.global["billbo99-secondary_ammo_starting_amount"].value}
     end
 
-    if settings.get_player_settings(player)["billbo99-respawn-with-capsule"] and global.SpawnItems["capsule"] then
-        player.insert {name = global.SpawnItems["capsule"], count = settings.global["billbo99-capsule_starting_amount"].value}
+    if settings.get_player_settings(player)["billbo99-respawn-with-capsule"] and ref["capsule"] then
+        player.insert {name = ref["capsule"], count = settings.global["billbo99-capsule_starting_amount"].value}
     end
 
-    if settings.get_player_settings(player)["billbo99-respawn-with-armor"] and global.SpawnItems["armor"] then
-        player.insert {name = global.SpawnItems["armor"], count = 1}
+    if settings.get_player_settings(player)["billbo99-respawn-with-armor"] and ref["armor"] then
+        player.insert {name = ref["armor"], count = 1}
     end
 end
 
@@ -125,67 +127,75 @@ local function InitCheckList()
 end
 
 function Spawn.OnTickDoCheckForSpawnGear()
-    global.SpawnItems.Checks = InitCheckList()
+    for _, force in pairs(game.forces) do
+        local ref = global.SpawnForceItems[force.name]
 
-    global.SpawnItems.primary_gun_threshold = settings.global["billbo99-primary_gun_threshold"].value
-    global.SpawnItems.secondary_gun_threshold = settings.global["billbo99-secondary_gun_threshold"].value
-    global.SpawnItems.primary_ammo_threshold = settings.global["billbo99-primary_ammo_threshold"].value
-    global.SpawnItems.secondary_ammo_threshold = settings.global["billbo99-secondary_ammo_threshold"].value
-    global.SpawnItems.armor_threshold = settings.global["billbo99-armor_threshold"].value
-    global.SpawnItems.capsule_threshold = settings.global["billbo99-capsule_threshold"].value
+        ref.Checks = InitCheckList()
 
-    local flag = false
-    for k, force in pairs(game.forces) do
+        ref.primary_gun_threshold = settings.global["billbo99-primary_gun_threshold"].value
+        ref.secondary_gun_threshold = settings.global["billbo99-secondary_gun_threshold"].value
+        ref.primary_ammo_threshold = settings.global["billbo99-primary_ammo_threshold"].value
+        ref.secondary_ammo_threshold = settings.global["billbo99-secondary_ammo_threshold"].value
+        ref.armor_threshold = settings.global["billbo99-armor_threshold"].value
+        ref.capsule_threshold = settings.global["billbo99-capsule_threshold"].value
+
+        local flag = false
         local produced = force["item_production_statistics"].input_counts
         -- for each type do a check  (gun/ammo/armor)
-        for k1, _ in pairs(global.SpawnItems.Checks) do
+        for k1, _ in pairs(ref.Checks) do
             -- for each item under that type see which one we should be using
-            local old_what_type = global.SpawnItems[k1]
-            for _, v2 in pairs(global.SpawnItems.Checks[k1]) do
-                if produced[v2.what] and produced[v2.what] > global.SpawnItems[v2.what_type .. "_threshold"] then
-                    global.SpawnItems[v2.what_type] = v2.what
-                    global.SpawnItems[v2.what_type .. "_name"] = v2.what_name
+            local old_what_type = ref[k1]
+            for _, v2 in pairs(ref.Checks[k1]) do
+                if produced[v2.what] and produced[v2.what] > ref[v2.what_type .. "_threshold"] then
+                    ref[v2.what_type] = v2.what
+                    ref[v2.what_type .. "_name"] = v2.what_name
                 end
             end
-            if old_what_type ~= global.SpawnItems[k1] then
+            if old_what_type ~= ref[k1] then
                 flag = true
             end
         end
 
         if settings.global["billbo99-get-starting-gear"].value then
-            if not global.SpawnItems.primary_gun_name then
-                global.SpawnItems["primary_gun"] = global.SpawnItems.Checks.primary_gun[1].what
-                global.SpawnItems["primary_gun_name"] = global.SpawnItems.Checks.primary_gun[1].what_name
+            if not ref.primary_gun_name then
+                ref["primary_gun"] = ref.Checks.primary_gun[1].what
+                ref["primary_gun_name"] = ref.Checks.primary_gun[1].what_name
             end
-            if not global.SpawnItems.primary_ammo_name then
-                global.SpawnItems["primary_ammo"] = global.SpawnItems.Checks.primary_ammo[1].what
-                global.SpawnItems["primary_ammo_name"] = global.SpawnItems.Checks.primary_ammo[1].what_name
+            if not ref.primary_ammo_name then
+                ref["primary_ammo"] = ref.Checks.primary_ammo[1].what
+                ref["primary_ammo_name"] = ref.Checks.primary_ammo[1].what_name
             end
         end
 
         if flag then
             local list = {}
-            if global.SpawnItems.armor_name then
-                table.insert(list, global.SpawnItems.armor_name)
+            if ref.armor_name then
+                table.insert(list, ref.armor_name)
             end
-            if global.SpawnItems.primary_gun_name then
-                table.insert(list, global.SpawnItems.primary_gun_name)
+            if ref.primary_gun_name then
+                table.insert(list, ref.primary_gun_name)
             end
-            if global.SpawnItems.primary_ammo_name then
-                table.insert(list, global.SpawnItems.primary_ammo_name)
+            if ref.primary_ammo_name then
+                table.insert(list, ref.primary_ammo_name)
             end
-            if global.SpawnItems.secondary_gun_name then
-                table.insert(list, global.SpawnItems.secondary_gun_name)
+            if ref.secondary_gun_name then
+                table.insert(list, ref.secondary_gun_name)
             end
-            if global.SpawnItems.secondary_ammo_name then
-                table.insert(list, global.SpawnItems.secondary_ammo_name)
+            if ref.secondary_ammo_name then
+                table.insert(list, ref.secondary_ammo_name)
             end
-            if global.SpawnItems.capsule_name then
-                table.insert(list, global.SpawnItems.capsule_name)
+            if ref.capsule_name then
+                table.insert(list, ref.capsule_name)
             end
-            force.print("Clones will now receive the following on respawn; " .. table.concat(list, ", "), global.print_colour)
+
+            local msg = "Clones will now receive the following on respawn; "
+            if force.name ~= "player" then
+                msg = "Clones from the force (" .. force.name .. ") will now receive the following on respawn; "
+            end
+            force.print(msg .. table.concat(list, ", "), global.print_colour)
         end
     end
+    log("Spawn.OnTickDoCheckForSpawnGear Completed")
 end
 
 function Spawn.OnConfigurationChanged(e)
@@ -199,35 +209,43 @@ function Spawn.OnInit()
     global.print_colour = {r = 255, g = 255, b = 0}
     global.SpawnTimer = global.SpawnTimer or {}
     global.SpawnItems = global.SpawnItems or {}
+    global.SpawnForceItems = global.SpawnForceItems or {}
     global.queue = global.queue or {}
     global.surprise = global.surprise or {flag = false, chance = 10}
 
-    -- global.SpawnItems.DeathCount = {}
-    global.SpawnItems.Checks = InitCheckList()
+    for _, force in pairs(game.forces) do
+        if global.SpawnForceItems[force.name] == nil then
+            global.SpawnForceItems[force.name] = {}
+        end
 
-    global.SpawnItems.primary_gun = global.SpawnItems.primary_gun or nil
-    global.SpawnItems.primary_gun_name = global.SpawnItems.primary_gun_name or nil
-    global.SpawnItems.primary_gun_priority = global.SpawnItems.primary_gun_priority or 0
+        local ref = global.SpawnForceItems[force.name]
+        ref.Checks = InitCheckList()
 
-    global.SpawnItems.primary_ammo = global.SpawnItems.primary_ammo or nil
-    global.SpawnItems.primary_ammo_name = global.SpawnItems.primary_ammo_name or nil
-    global.SpawnItems.primary_ammo_priority = global.SpawnItems.primary_ammo_priority or 0
+        ref.primary_gun = ref.primary_gun or nil
+        ref.primary_gun_name = ref.primary_gun_name or nil
+        ref.primary_gun_priority = ref.primary_gun_priority or 0
 
-    global.SpawnItems.secondary_gun = global.SpawnItems.secondary_gun or nil
-    global.SpawnItems.secondary_gun_name = global.SpawnItems.secondary_gun_name or nil
-    global.SpawnItems.secondary_gun_priority = global.SpawnItems.secondary_gun_priority or 0
+        ref.primary_ammo = ref.primary_ammo or nil
+        ref.primary_ammo_name = ref.primary_ammo_name or nil
+        ref.primary_ammo_priority = ref.primary_ammo_priority or 0
 
-    global.SpawnItems.secondary_ammo = global.SpawnItems.secondary_ammo or nil
-    global.SpawnItems.secondary_ammo_name = global.SpawnItems.secondary_ammo_name or nil
-    global.SpawnItems.secondary_ammo_priority = global.SpawnItems.secondary_ammo_priority or 0
+        ref.secondary_gun = ref.secondary_gun or nil
+        ref.secondary_gun_name = ref.secondary_gun_name or nil
+        ref.secondary_gun_priority = ref.secondary_gun_priority or 0
 
-    global.SpawnItems.capsule = global.SpawnItems.capsule or nil
-    global.SpawnItems.capsule_name = global.SpawnItems.capsule_name or nil
-    global.SpawnItems.capsule_priority = global.SpawnItems.capsule_priority or nil
+        ref.secondary_ammo = ref.secondary_ammo or nil
+        ref.secondary_ammo_name = ref.secondary_ammo_name or nil
+        ref.secondary_ammo_priority = ref.secondary_ammo_priority or 0
 
-    global.SpawnItems.armor = global.SpawnItems.armor or nil
-    global.SpawnItems.armor_name = global.SpawnItems.armor_name or nil
-    global.SpawnItems.armor_priority = global.SpawnItems.armor_priority or 0
+        ref.capsule = ref.capsule or nil
+        ref.capsule_name = ref.capsule_name or nil
+        ref.capsule_priority = ref.capsule_priority or nil
+
+        ref.armor = ref.armor or nil
+        ref.armor_name = ref.armor_name or nil
+        ref.armor_priority = ref.armor_priority or 0
+    end
+    log("Spawn.OnInit Complete")
 end
 
 -- Register default commands
@@ -242,44 +260,55 @@ function Spawn.OnRuntimeModSettingChanged(event)
     end -- not a setting we care about presently
 
     -- lazy time
-    global.SpawnItems.primary_gun_threshold = settings.global["billbo99-primary_gun_threshold"].value
-    global.SpawnItems.secondary_gun_threshold = settings.global["billbo99-secondary_gun_threshold"].value
-    global.SpawnItems.primary_ammo_threshold = settings.global["billbo99-primary_ammo_threshold"].value
-    global.SpawnItems.secondary_ammo_threshold = settings.global["billbo99-secondary_ammo_threshold"].value
-    global.SpawnItems.armor_threshold = settings.global["billbo99-armor_threshold"].value
+    for _, force in pairs(game.forces) do
+        local ref = global.SpawnForceItems[force.name]
+        ref.primary_gun_threshold = settings.global["billbo99-primary_gun_threshold"].value
+        ref.secondary_gun_threshold = settings.global["billbo99-secondary_gun_threshold"].value
+        ref.primary_ammo_threshold = settings.global["billbo99-primary_ammo_threshold"].value
+        ref.secondary_ammo_threshold = settings.global["billbo99-secondary_ammo_threshold"].value
+        ref.armor_threshold = settings.global["billbo99-armor_threshold"].value
+    end
 end
 
 function Spawn.Recalculate(event)
     local player = game.get_player(event.player_index)
+    local force = player.force
     if not player.admin then
         player.print("Your not an admin")
         return
     end
 
-    global.SpawnItems.Checks = InitCheckList()
+    global.SpawnForceItems[force.name].Checks = InitCheckList()
     Spawn.OnTickDoCheckForSpawnGear()
 end
 
 function Spawn.Reprint(event)
     local player = game.get_player(event.player_index)
+    local force = player.force
+    local ref = global.SpawnForceItems[force.name]
 
     local list = {}
-    if global.SpawnItems.armor_name then
-        table.insert(list, global.SpawnItems.armor_name)
+    if ref.armor_name then
+        table.insert(list, ref.armor_name)
     end
-    if global.SpawnItems.primary_gun_name then
-        table.insert(list, global.SpawnItems.primary_gun_name)
+    if ref.primary_gun_name then
+        table.insert(list, ref.primary_gun_name)
     end
-    if global.SpawnItems.primary_ammo_name then
-        table.insert(list, global.SpawnItems.primary_ammo_name)
+    if ref.primary_ammo_name then
+        table.insert(list, ref.primary_ammo_name)
     end
-    if global.SpawnItems.secondary_gun_name then
-        table.insert(list, global.SpawnItems.secondary_gun_name)
+    if ref.secondary_gun_name then
+        table.insert(list, ref.secondary_gun_name)
     end
-    if global.SpawnItems.secondary_ammo_name then
-        table.insert(list, global.SpawnItems.secondary_ammo_name)
+    if ref.secondary_ammo_name then
+        table.insert(list, ref.secondary_ammo_name)
     end
-    player.print("Clones will now receive the following on respawn; " .. table.concat(list, ", "), global.print_colour)
+    player.print(force.name)
+    local msg = "Clones will now receive the following on respawn; "
+    if force.name ~= "player" then
+        msg = "Clones from the force (" .. force.name .. ") will now receive the following on respawn; "
+    end
+    player.print(msg .. table.concat(list, ", "), global.print_colour)
 end
 
 function Spawn.RespawnTime(event)
@@ -310,7 +339,8 @@ end
 
 function Spawn.OnPlayerRespawned(event)
     local player = game.get_player(event.player_index)
-    global.SpawnItems.Checks = InitCheckList()
+    local force = player.force
+    global.SpawnForceItems[force.name].Checks = InitCheckList()
     Spawn.OnTickDoCheckForSpawnGear()
 
     Spawn.GiveGear(event)
